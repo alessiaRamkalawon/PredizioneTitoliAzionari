b2552dc7af65b13a19d29b880c4a2b8ea2621f74
Alessia Ramkalawon <61582315+alessiaRamkalawon@users.noreply.github.com>
2022-02-01 23:30:23 +0100

    valutazione

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as data
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error,mean_absolute_error,mean_absolute_percentage_error
 from keras.models import load_model
 import streamlit as st
 
@@ -14,7 +15,7 @@ input = st.text_input('Inserisci un titolo azionario', 'AAPL')
 #leggo i dati da yahoo finance
 df = data.DataReader(input,'yahoo',dataInizio,dataFine)
 
-#Descrizione del df
+#Descrizione del dataframe
 st.subheader('Dati nel periodo 2010 - 2019')
 st.write(df.describe())
 
@@ -34,7 +35,7 @@ plt.plot(ma200, 'g')
 plt.plot(df.Close, 'b')
 st.pyplot(fig)
 
-#divido i datri in train e test
+#divido i dati in train e test
 dataTraining = pd.DataFrame(df['Close'][0:int(len(df)*0.70)])
 dataTesting = pd.DataFrame(df['Close'][0:int(len(df)*0.30)])
 
@@ -58,8 +59,14 @@ for i in range(100,inputData.shape[0]):
 x_test , y_test = np.array(x_test), np.array(y_test)
 #Ora faccio le predizioni
 y_predicted = model.predict(x_test)
-print(scaler.scale_)
+
+print('fattore di scala: ' + str(scaler.scale_[0]))
 scaleFactor = 1/scaler.scale_[0]
+y_predicted = y_predicted * scaleFactor
+y_test = y_test * scaleFactor
+
+print('errore assoluto medio: ' + str(mean_absolute_error(y_test,y_predicted)))
+print('erroe assoluto medio percentuale: ' + str((mean_absolute_error(y_test,y_predicted)/y_test.mean())*100))
 
 #Grafico finale
 st.subheader('Predizioni vs Originari')
